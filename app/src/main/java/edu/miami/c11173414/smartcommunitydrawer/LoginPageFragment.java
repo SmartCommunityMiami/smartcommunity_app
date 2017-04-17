bddc6129335ac4f2c00e403d936d64b443b01a3c
Blake Maune <bfm7@miami.edu>
2017-04-17 16:48:04 -0400

    Added new method to LoginPageFragment; needs work

--- app/src/main/java/edu/miami/c11173414/smartcommunitydrawer/LoginPageFragment.java
+++ app/src/main/java/edu/miami/c11173414/smartcommunitydrawer/LoginPageFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.HttpConnection;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginPageFragment extends Fragment implements View.OnClickListener {
     View fragmentLayout;
+
+
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         fragmentLayout = inflater.inflate(R.layout.fragment_login_page, container,false);
 
@@ -27,6 +48,72 @@ public class LoginPageFragment extends Fragment implements View.OnClickListener 
         return(fragmentLayout);
     }
 
+    private boolean sendLoginRequest(String username, String password){
+        try {
+            // Figure out what this needs
+            String urlParameters = "username=" + username + "&password=" + password;
+
+            byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+            int postDataLength = postData.length;
+
+            String requestURL = "http://smartcommunity-dev.us-east-1.elasticbeanstalk.com/login";
+            URL url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            conn.setUseCaches(false);
+
+            try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+                wr.write(postData);
+            }
+        }catch (Exception e){
+            Log.i("sendLoginRequest: ", "Something went wrong");
+        }
+        return false;
+    }
+
+    private boolean sendLoginRequestApache(String username, String password){
+        HttpClient httpClient;
+        HttpPost httppost;
+
+        httpClient = new DefaultHttpClient();
+        httppost = new HttpPost("http://smartcommunity-dev.us-east-1.elasticbeanstalk.com/users");
+
+        // Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+            //Execute and get the response.
+            HttpResponse response = httpClient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                try {
+                    return true;
+                } finally {
+                    instream.close();
+                }
+            } else {
+                Log.i("sendLoginRequest:", "HTTP Response Null");
+            }
+        }catch (Exception e){
+            Toast.makeText(getActivity(), "Something went wrong with the login request!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return false;
+    }
+
     @Override
     public void onClick(View view) {
         EditText usernameField, passwordField;
@@ -35,7 +122,7 @@ public class LoginPageFragment extends Fragment implements View.OnClickListener 
         String userName, password;
         userName = usernameField.getText().toString();
         password = passwordField.getText().toString();
-        Toast.makeText(getActivity(), "U: " + userName + " P: " + password +"", Toast.LENGTH_SHORT).show();
+        Log.i("onClick:", "U: " + userName + " P: " + password);
     }
 
 }
