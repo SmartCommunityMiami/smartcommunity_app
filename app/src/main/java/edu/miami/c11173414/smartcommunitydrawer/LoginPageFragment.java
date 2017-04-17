e385126a2c21c49a416ab97a984bffb18926f87c
Conor Murray <conormurray@Conors-MacBook-Pro.local>
2017-04-17 16:07:46 -0400

    another httpclient library tweak

--- app/src/main/java/edu/miami/c11173414/smartcommunitydrawer/LoginPageFragment.java
+++ app/src/main/java/edu/miami/c11173414/smartcommunitydrawer/LoginPageFragment.java
@@ -9,6 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginPageFragment extends Fragment implements View.OnClickListener {
     View fragmentLayout;
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +45,35 @@ public class LoginPageFragment extends Fragment implements View.OnClickListener 
         userName = usernameField.getText().toString();
         password = passwordField.getText().toString();
         Toast.makeText(getActivity(), "U: " + userName + " P: " + password +"", Toast.LENGTH_SHORT).show();
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("http://smartcommunity-dev.us-east-1.elasticbeanstalk.com/users/new");
+
+// Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("param-1", "12345"));
+        params.add(new BasicNameValuePair("param-2", "Hello!"));
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+        } catch (Exception e) {
+
+        }
+
+//Execute and get the response.
+        try {
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                try {
+                    // do something useful
+                } finally {
+                    instream.close();
+                }
+            }
+        } catch (Exception e) {
+
+        }
     }
 
 }
